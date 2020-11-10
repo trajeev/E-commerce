b3e4882a67dc0e82b3903f7c6a8bdcd8b503921b
Rajeev Tikka <rajeevtikka@yahoo.com>
2020-11-10 12:51:32 -0500

    Included WithRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx'
+import {Route, Switch} from 'react-router-dom'
+
+const Hatspage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path = '/' component = {HomePage} />
+        <Route path = '/hats' component = {Hatspage} />
+      </Switch>
     </div>
   );
 }
